a9cfbf6c4714a75ab86ceb06f60a22bfbcdbd7f3
parker mcleod <parkercmcleod@gmail.com>
2024-04-02 06:35:21 -0400

    removed linked list for bag, only for chain

--- birthday_presents_party.cc
+++ birthday_presents_party.cc
@@ -9,17 +9,23 @@ Due date: Thursday, April 13th by 11:59 PM
 #include <vector>
 #include <algorithm>
 #include <random>
+#include <optional>
+#include <mutex>
 
-#define N 500'000 // C++ 14 notation!
+#define N 500'000
 
 struct Present {
     unsigned int tag;
-    Present* next;
+};
+
+struct PresentChain {
+    unsigned int tag;
+    Present* next = nullptr;
 };
 
 int main(void) {
 
-    /************************** CREATE GUEST PRESENTS **************************/
+    /******************************* CREATE GUEST PRESENTS *******************************/
 
     // There are N guest presents
     std::vector<Present> presents(N);
@@ -30,28 +36,26 @@ int main(void) {
 
     /************************** PLACE PRESENTS IN UNORDERED BAG **************************/
 
-    // Place the N guests' presents into the unordered bag
-    Present* unorderedBag[N] = {};
+    // Place the N guests' presents into the bag
+    std::vector<Present*> unorderedBag(N);
+    for (int i = 0; i < N; ++i) {
+        unorderedBag[i] = &presents[i];
+    }
 
-    // Seed random number gen
+    // Shuffle presents in bag
     std::random_device rd;
     std::mt19937 g(rd());
+    std::shuffle(unorderedBag.begin(), unorderedBag.end(), g);
 
-    // Efficient random shuffling of 0 to N
-    std::shuffle(presents.begin(), presents.end(), g);
-
-    // Assign each present in chain after they have been swapped up
-    for (unsigned int i = 0; i < (N-1); ++i) {
-        unorderedBag[i] = &presents[i];
-        unorderedBag[i]->next = &presents[i+1];
-    }
-    unorderedBag[N-1]->next = NULL;
-
-    // Display the first then presents pulled out of the bag
+    // Display the first 10 presents pulled out of the bag
     for (int i = 0; i < 10; ++i) {
         std::cout << unorderedBag[i]->tag << std::endl;
     }
 
+    /****************************** PLACE PRESENTS ON CHAIN ******************************/
+
+    // Present chain
+    std::vector<PresentChain*> presentChain(N);
 
     return 0;
 }
